4b0e901a67d1e0897731c04a659944ea1b70b08c
JosHien <jos.hiennguyen@gmail.com>
2015-11-27 18:00:01 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'dangrossman:bootstrap-daterangepicker',
+  name: 'hiennguyen:bootstrap-daterangepicker',
   version: '2.1.13',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
@@ -9,9 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
 
-  api.use('twbs:bootstrap@3.3.4', ["client"]);
-  api.use('momentjs:moment@2.10.3', ["client"]);
-  api.use('jquery@1.11.3_2', ["client"]);
+  api.use(['jquery', 'moment'], "client", {weak:true})
 
   api.addFiles('daterangepicker.js', ["client"]);
   api.addFiles('daterangepicker.css', ["client"]);
